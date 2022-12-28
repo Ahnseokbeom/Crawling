61e9fb1831051bbe5dc4c3f3525e3998f25af584
seobkeom <ajm153123@naver.com>
2022-12-28 15:02:36 +0900

    movie detail crawling code update

--- src/Movie/detail.java
+++ src/Movie/detail.java
@@ -1,6 +1,9 @@
 package Movie;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.Arrays;
 
 import org.jsoup.Jsoup;
@@ -19,18 +22,49 @@ public class detail {
 			detail[i++] = "http://www.cgv.co.kr"+e.attr("href");
 			}
 		}
-		System.out.println(Arrays.toString(detail));
-		Document doc2 = Jsoup.connect(detail[0]).get();
-		Elements a = doc2.select("div.spec");
-		Elements details = doc2.select("div.sect-story-movie");
-		System.out.println(a.text());
-		String[] a2 = a.text().split("/");
-		String[] a3 = a2[1].split(" 장르 :");
-		System.out.println(Arrays.toString(a2));
-		System.out.println(Arrays.toString(a3));
-		System.out.println(a3[1]);
-//		System.out.println(a2[1]);
+		try {
+			String director = "";
+			String actor = "";
+			String genre = "";
+			String deta = "";
+			String sql = "";
+			java.sql.Statement st = null;
+//			ResultSet rs = null;
+			Connection con = null;
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/?serverTimezone=UTC&useSSL=false &allowPublicKeyRetrieval=true",
+					"root", "4612");
+			st = con.createStatement();
+			st.executeUpdate("use movie;");
+			for(i = 0;i<19;i++) {
+				Document doc2 = Jsoup.connect(detail[i]).get();
+				Elements a = doc2.select("div.spec");
+				Elements details = doc2.select("div.sect-story-movie");
+				String[] a2 = a.text().split("/ 배우 : ");
+				String[] a3 = a2[1].split(" 장르 : ");
+				String[] a4 = a3[1].split(" / ");
+				director = a2[0];
+				actor = a3[0];
+				genre = a4[0];
+				deta = details.text();
+//				sql = "insert into Game values(?,?,?,?,?)";
+				sql = "insert into detail values(?,?,?,?,?)";
+				PreparedStatement pst = con.prepareStatement(sql);
+				System.out.println(i+1);
+				pst.setInt(1, i+1);
+				pst.setString(2, director);
+				pst.setString(3, actor);
+				pst.setString(4, genre);
+				pst.setString(5, deta);
+				pst.execute();
+				pst.close();
+				System.out.println("direcort : "+director);
+				System.out.println("actor : "+actor);
+				System.out.println("genre : "+genre);
+				System.out.println("deta : "+deta);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		
-		System.out.println(details.text());
 	}
 }
